6104ee3561caa0ccda8d3c6862aeea16926e488b
Kristian Nissen <kristian.nissen@gmail.com>
2019-03-15 13:24:28 +0100

    Working on #1

--- resources/js/components/BlogEntryList.vue
+++ resources/js/components/BlogEntryList.vue
@@ -2,15 +2,39 @@
  * @file BlogEntryList.vue
  */
 <template>
-    <div>I am nasty</div>    
+<div>
+    <div class="mdl-card mdl-cell mdl-cell--8-col" v-for="item in list">
+        <div class="mdl-card__title">
+            <h3>{{item.title}}</h3>
+        </div>
+        <div class="mdl-card__supporting-text">
+            {{item.content}}
+        </div>
+        <div class="mdl-card__supporting-text">meta</div>
+    </div>
+</div>
 </template>
 
 <script>
     export default {
+        data() {
+            return { 
+                list: [],
+                title: ''
+            }
+        },
+        methods: {
+            fetchBlogEntryList: function () {
+                var endPoint = '/api/blogentries/'
+                this.http.get(endPoint)
+                .then((response) => this.list = response.data.data)
+            }
+        },
         mounted() {
             console.log('Mounted')
         },
         created(){
+            this.fetchBlogEntryList()
             console.log('Created')
         }
     };
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
    <div class="mdl_grid blogentries" id="app">
+        <blogentrylist />
         <div class="mdl-card mdl-cell mdl-cell--8-col">
             <div class="mdl-card__title">
                 <h3>Hello Kitty</h3>
             </div>
             <div class="mdl-card__supporting-text">
-                <blogentrylist />
             </div>
             <div class="mdl-card__supporting-text">meta</div>
         </div>
