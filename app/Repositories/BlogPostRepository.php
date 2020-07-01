14f1d7238215be7bc129403e47b04ff55502f889
Kristian Nissen <kristian.nissen@gmail.com>
2020-07-01 23:38:02 +0200

    Fixes #32 for good

--- app/Repositories/BlogPostRepository.php
+++ app/Repositories/BlogPostRepository.php
@@ -19,27 +19,29 @@ class BlogPostRepository implements BlogPostRepositoryInterface
 
     public function find(string $slug)
     {
-        $blog_post = Cache::get($slug, function () use ($slug) {
+        if (!Cache::has($slug)) {
             $this->model = $this->model->where('slug', '=', $slug)->first();
             $image_files = ImageFile::where('model_name', '=', 'BlogPost')
                 ->where('model_id', '=', $this->model->id)
                 ->where('file_size', '!=', 'original')
                 ->get();
             $blog_post_images = [
-              'hero_images' => [],
-              'slider_images' => []
+                'hero_images' => [],
+                'slider_images' => [],
             ];
             foreach ($image_files as $image_file) {
-              if ($image_file->role == 'hero_image') {
-                array_push($blog_post_images['hero_images'], [
-                  'breakpoint_'. $image_file->file_size =>
-                  $image_file->file_name
-                ]);
-              }
+                if ($image_file->role == 'hero_image') {
+                    array_push($blog_post_images['hero_images'], [
+                        'breakpoint_' .
+                        $image_file->file_size => $image_file->file_name,
+                    ]);
+                }
             }
             $this->model->hero_images = $blog_post_images['hero_images'];
-            return $this->model;
-        });
-        return $blog_post;
+            Cache::put($slug, $this->model, now()->addMinutes(10));
+        } else {
+            $this->model = Cache::get($slug);
+        }
+        return $this->model;
     }
 }
